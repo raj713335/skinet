8bfe73ed41c589cb592fa8e8830716b287e2e7bc
ARNAB DAS <raj713335@gmail.com>
2022-04-05 10:14:01 +0530

    149. Basket Service methods

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { IBasket } from '../shared/models/basket';
 
@@ -9,8 +10,31 @@ import { IBasket } from '../shared/models/basket';
 })
 export class BasketService {
 
-  // baseUrl = environment.apiUrl;
-  // private basketSource = new BehaviorSubject<IBasket>(null);
+  baseUrl = environment.apiUrl;
+  private basketSource = new BehaviorSubject<IBasket>(null);
+
+  basket$ = this.basketSource.asObservable();
 
   constructor(private http: HttpClient) { }
+
+  getBasket(id: string) {
+    return this.http.get(this.baseUrl + 'basket?id='+ id)
+    .pipe(
+      map((basket: IBasket) => {
+        this.basketSource.next(basket);
+      })
+    );
+  }
+
+  setBasket(basket: IBasket) {
+    return this.http.post(this.baseUrl + 'basket', basket).subscribe(( response : IBasket) => {
+      this.basketSource.next(response);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  getCurrentBasketValue(){
+    return this.basketSource.value;
+  }
 }
